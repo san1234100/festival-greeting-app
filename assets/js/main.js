4391d6e1ac4a3e1ca2a6e026f9fd503b92efe33d
Sanjeev U <152139380+san1234100@users.noreply.github.com>
2024-02-17 17:23:08 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,5 @@ console.log(formEl);
 formEl.addEventListener("submit",(e)=>{
 e.preventDefault();
 const greeterName=e.target[0].value;
-window.location.href="http://localhost:5174/festival-greeting-app/greeting.html?name="+greeterName;
+window.location.href="/festival-greeting-app/greeting.html?name="+greeterName;
 })
